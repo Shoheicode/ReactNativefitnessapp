14075cf9c97af4e44f2e07ddf216fa16adc260fc
ShoheiCode <ucodejasonirie@gmail.com>
2024-08-29 23:44:05 -0700

    deleted pages but adding home page

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,44 @@
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { heightPercentageToDP } from "react-native-responsive-screen";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home(){
     return (
-        <View>
-            <Text>
-                Home
-            </Text>
-        </View>
+        <SafeAreaView>
+            <StatusBar style="dark" />
+            {/* punchline and avatar*/}
+            <View
+                style={
+                    {
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginLeft: 20,
+                        marginRight: 20,
+                    }
+                }
+            >
+                <View
+                    style={
+                        {
+                            marginTop: 0.5,
+                        }
+                    }
+                >
+                    <Text
+                        style={{
+                            fontSize: heightPercentageToDP(4.5),
+                            fontWeight: "bold",
+                            
+                        }}
+                    >
+                        Ready To
+                    </Text>
+
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
