2160daa07d01018e56fa3365b6623d13309ba031
ShoheiCode <ucodejasonirie@gmail.com>
2024-08-29 22:31:39 -0700

    Added images and updated page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,52 +1,52 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View, Dimensions, Animated } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import AnimatedGradientBackground from '@/components/Background';
+import { useEffect, useRef } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function HomeScreen() {
+
+  const { width, height } = Dimensions.get('window');
+
+  const styl = StyleSheet.create({
+    container: {
+      flex: 1,
+      width: width,
+      height: height,
+      justifyContent: "flex-end"
+    },
+  });
+
+  const animation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const animateGradient = () => {
+      Animated.timing(animation, {
+        toValue: 1,
+        duration: 5000,
+        useNativeDriver: false,
+      }).start(() => {
+        animation.setValue(0);
+        animateGradient();
+      });
+    };
+
+    animateGradient();
+  }, []);
+
+  const colorInterpolation = animation.interpolate({
+    inputRange: [0, 0.5, 1],
+    outputRange: ['rgba(131,58,180,1)', 'rgba(253,29,29,1)', 'rgba(252,176,69,1)'],
+  });
+  
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <View style={styl.container}>
+        <StatusBar style='light'/>
+      </View>
   );
 }
 
@@ -54,6 +54,7 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: "center",
     gap: 8,
   },
   stepContainer: {
