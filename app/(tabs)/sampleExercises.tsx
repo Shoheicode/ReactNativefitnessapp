ce1f01b1287aabea6dd9c1c0a5919108b7e7fbfb
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-03 18:18:03 -0700

    Updated Sample exerices and exercise section

---
+++ app/(tabs)/sampleExercises.tsx
@@ -0,0 +1,139 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, Image, Text, View, StyleSheet } from "react-native";
+import { heightPercentageToDP, widthPercentageToDP } from "react-native-responsive-screen";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import ImageSlider from "@/components/ImageSlider";
+import { SignedIn, useUser, SignedOut, useAuth } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
+import CoolSignInButton from "@/components/CoolSignInButton";
+import ExerciseSections from "@/components/ExerciseSections"
+import { Avatar } from '@rneui/themed';
+
+export default function SampleExercises(){
+    const size = 40
+
+    const { user } = useUser()
+    const { signOut } = useAuth()
+
+    const handleLogout = async () => {
+        try {
+          await signOut();
+          // You can add additional logic here, such as navigation to a login screen
+          console.log('User logged out successfully');
+        } catch (error) {
+          console.error('Error logging out:', error);
+        }
+      };
+
+    return (
+        <SafeAreaView>
+            {/* <StatusBar style="dark" /> */}
+            {/* punchline and avatar*/}
+            <View
+                style={
+                    {
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginLeft: 20,
+                        marginRight: 20,
+                    }
+                }
+            >
+                <View
+                    style={
+                        {
+                            marginTop: 0.5,
+                        }
+                    }
+                >
+                    <Text
+                        style={{
+                            fontSize: heightPercentageToDP(4.5),
+                            fontWeight: "bold",
+                            color: "grey",
+                            letterSpacing: 1,
+                            marginTop: 0.5,
+                        }}
+                    >
+                        READY TO
+                    </Text>
+                    <Text
+                        style={{
+                            fontSize: heightPercentageToDP(4.5),
+                            fontWeight: "bold",
+                            color: "red",
+                            letterSpacing: 1,
+                            marginTop: 0.5,
+                        }}
+                    >
+                        WORKOUT
+                    </Text>
+
+                </View>
+                <View
+                    style={
+                        {
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            marginTop: 0.5,
+                        }
+                    }
+                >
+                    <SignedOut>
+                        <CoolSignInButton
+                        onPress={()=>router.push('/(auth)/sign-in')}
+                        
+                        />
+                        <CoolSignInButton
+                            onPress={()=>router.push('/(auth)/sign-up')}
+                            title="Sign Up"
+                        />
+                    </SignedOut>
+                    <SignedIn>
+                        <Text
+                            style={
+                                {
+                                    marginBottom: 10,
+                                    fontSize:heightPercentageToDP(2)
+                                }
+                            }
+                        >Welcome {user?.fullName}</Text>
+
+                        <CoolSignInButton 
+                            onPress={handleLogout}
+                            title="Log Out"    
+                        />
+                    </SignedIn>
+                </View>
+            </View>
+
+            <SignedIn>
+                <View
+                    style={
+                        {
+                            display:"flex"
+                        }
+                    }
+                >
+                    <ExerciseSections/>
+                </View>
+            </SignedIn>
+        </SafeAreaView>
+    )
+}
+const styles = StyleSheet.create({
+    placeholder: {
+      backgroundColor: '#e1e1e1',
+      borderRadius: 20,
+    },
+    iconContainer: {
+      backgroundColor: '#4a90e2',
+      borderRadius: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  })
---
+++ components/ExerciseSections.jsx
@@ -0,0 +1,124 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { router } from "expo-router";
+import { FlatList, Text, TouchableOpacity,Image } from "react-native";
+import { View } from "react-native";
+import { heightPercentageToDP, widthPercentageToDP } from "react-native-responsive-screen";
+
+const list = [
+    {
+        name: "Chat Bot",
+        source: require("../assets/images/profileimages/robotExercising.png"),
+        route: '/(tabs)/chatBot',
+    },
+    {
+        name: "Saved Exercises",
+        source: require("../assets/images/exerciseImages/back.png"),
+        route: '/(tabs)/savedExercises',
+    },
+    {
+        name: "Programs",
+        source: require("../assets/images/exerciseImages/back.png"),
+        route: '/(tabs)/savedExercises',
+    },
+    {
+        name: "Sample Exercises",
+        source: require("../assets/images/exerciseImages/back.png"),
+        route: '/(tabs)/savedExercises',
+    }
+]
+
+export default function ExerciseSections(){
+    return (
+        <View
+            style={
+                {
+                    height: heightPercentageToDP(50),
+                    display: "flex",
+                    //flex: auto,
+                    //width: widthPercentageToDP(100)
+                    marginLeft: widthPercentageToDP(3),
+                    marginRight: widthPercentageToDP(3)
+                }
+            }
+        >
+            <Text
+                style={
+                    {
+                        fontSize: heightPercentageToDP(3)
+                    }
+                }
+            >
+                Options:
+            </Text>
+
+            <FlatList
+                data={list}
+                numColumns={2}
+                keyExtractor={item=>item.name}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom:50, paddingTop: 20}}
+                columnWrapperStyle={{
+                    justifyContent:"space-between"
+                }}
+                renderItem={({item, index}) => <GridCard index = {index} item = {item}/>}
+            />
+        </View>
+    )
+}
+
+const GridCard = ({index, item})=>{
+    return (
+        <View>
+            <TouchableOpacity
+                style={
+                    {
+                        width:widthPercentageToDP(44),
+                        height: widthPercentageToDP(52),
+                        display: "flex",
+                        justifyContent: "flex-end",
+                        marginTop: heightPercentageToDP(2)
+                    }
+                }
+                onPress={()=> router.push(item.route)}
+            >
+                <Image
+                    source={item.source}
+                    style={
+                        {
+                            width:widthPercentageToDP(44),
+                            height: widthPercentageToDP(52),
+                            borderRadius: 40,
+                            position:"absolute",
+                        }
+                    }
+                    resizeMode="cover"
+                />
+                <LinearGradient
+                    colors={['transparent', 'rgba(0,0,0,0.9)']}
+                    style={{
+                        width: widthPercentageToDP(44), 
+                        height: heightPercentageToDP(15),
+                        position: "absolute",
+                        bottom: 0,
+                        borderRadius: 40
+                    }}
+                    start={{x: 0, y:0}}
+                    end={{x: 0, y:1}}
+                    
+                />
+                <Text
+                    style={
+                        {
+                            color:"white",
+                            fontSize:heightPercentageToDP(2.3),
+                            fontWeight:"bold",
+                            textAlign:"center"
+                        }
+                    }
+                >
+                    {item.name}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
